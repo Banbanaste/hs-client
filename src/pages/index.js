267cdbea743d233a3ab704a2bcfa0152be6e597d
Roberto Banbanaste <banbanaste@Robertos-MacBook-Pro.local>
2022-01-23 15:26:57 -0500

    spa | akagi imge fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,30 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+const IndexPage = () => {
+  return (
+    <div className='w-container h-screen mx-auto px-8 relative flex justify-between font-hs-mono'>
+      <div className='w-118 mt-32'>
+        <h1 className='text-6xl font-hs-mono m-0'>Welcome to,<br /> Hacker Sunday</h1>
+        <p className='text-slate-400'>Remote hackathon every Sunday</p>
+        <h2 className='font-hs-mono capitalize m-0 mb-4'>About</h2>
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hacker Sunday, under dev</h1>
-    <p>
-      <Link to="/about">Go to About</Link> <br />
-      <Link to="/history">Go to History</Link> <br />
-      <Link to="/links">Go to Links</Link> <br />
-    </p>
-  </Layout>
-)
+        <div className='mb-8 text-sm'>
+          <p>Founded in 2016, we are a collective of hackers who host a weekly hack-a-thon on Sundays. We band together to explore our ideas and hack them into existence. We are developers, artists, students, and friends. Hacker Sunday is supposed to be a sustainable hack-a-thon experience, unlike your typical 24hr hack-a-thon. </p>
+          <p>There’s no specific topic except you can’t hack on “work” projects. Hacker Sunday is a place where you can explore freely with the support of your peers. We want to set aside Sundays for you to try things out and genuinely do cool shit. Not do more work for your corporate overlords. </p>
+        </div>
+
+        <p className='m-0 capitalize text-blue-700'><Link to="">Become a Hacker and Join Us</Link></p>
+        <p className="text-slate-400 text-xs">Join our Discord Server and add the Hacker Sunday Role</p>
+
+      </div>
+      <div className='w-104 mt-64 relative flex justify-center'>
+        <div className='w-56 absolute absolute -translate-y-32 z-10'><StaticImage className='animate-spin-slow' src="../images/circular_text.webp" alt="circular text" /></div>
+        <div className='w-104 absolute top-0'><StaticImage src="../images/akagi.webp" alt="akagi looking all contemplative" /></div>
+      </div>
+    </div>
+  )
+}
 
 export default IndexPage
